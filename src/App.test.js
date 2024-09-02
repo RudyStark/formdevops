9782ee9fb09fb171d59f8328ed7a93cdd8cce474
Lobodzinskaya Lika <lobodzinskaya.lika@gmail.com>
2024-09-02 16:19:31 +0200

    toptop

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+   import { render, screen } from '@testing-library/react';
+   import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+   test('renders login form', () => {
+     render(<App />);
+     const titleElement = screen.getByText(/Connexion/i);
+     expect(titleElement).toBeInTheDocument();
+     
+     const emailInput = screen.getByLabelText(/Email:/i);
+     expect(emailInput).toBeInTheDocument();
+     
+     const passwordInput = screen.getByLabelText(/Mot de passe:/i);
+     expect(passwordInput).toBeInTheDocument();
+     
+     const submitButton = screen.getByText(/Se connecter/i);
+     expect(submitButton).toBeInTheDocument();
+   });
