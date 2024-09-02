d42b6b3587e83e132a19bcd3c575c5a215c5eb08
Lobodzinskaya Lika <lobodzinskaya.lika@gmail.com>
2024-09-02 16:50:29 +0200

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,39 @@
 import React from 'react';
-   import { render, screen } from '@testing-library/react';
-   import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
 
-   test('renders login form', () => {
-     render(<App />);
-     const titleElement = screen.getByText(/Connexion/i);
-     expect(titleElement).toBeInTheDocument();
-     
-     const emailInput = screen.getByLabelText(/Email:/i);
-     expect(emailInput).toBeInTheDocument();
-     
-     const passwordInput = screen.getByLabelText(/Mot de passe:/i);
-     expect(passwordInput).toBeInTheDocument();
-     
-     const submitButton = screen.getByText(/Se connecter/i);
-     expect(submitButton).toBeInTheDocument();
-   });
+const generateRandomEmail = () => {
+  const randomString = Math.random().toString(36).substring(2, 10);
+  return `user_${randomString}@example.com`;
+};
+
+const generateRandomPassword = () => {
+  return Math.random().toString(36).substring(2, 10);
+};
+
+test('renders login form and handles input', () => {
+  render(<App />);
+  
+  const titleElement = screen.getByText(/Connexion/i);
+  expect(titleElement).toBeInTheDocument();
+  
+  const emailInput = screen.getByLabelText(/Email:/i);
+  expect(emailInput).toBeInTheDocument();
+  expect(emailInput).toHaveAttribute('type', 'email');
+  const randomEmail = generateRandomEmail();
+  fireEvent.change(emailInput, { target: { value: randomEmail } });
+  expect(emailInput).toHaveValue(randomEmail);
+  
+  const passwordInput = screen.getByLabelText(/Mot de passe:/i);
+  expect(passwordInput).toBeInTheDocument();
+  expect(passwordInput).toHaveAttribute('type', 'password');
+  const randomPassword = generateRandomPassword();
+  fireEvent.change(passwordInput, { target: { value: randomPassword } });
+  expect(passwordInput).toHaveValue(randomPassword);
+  
+  const submitButton = screen.getByText(/Se connecter/i);
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton).toHaveAttribute('type', 'submit');
+  
+  fireEvent.click(submitButton);
+});
